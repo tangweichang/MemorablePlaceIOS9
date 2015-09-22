d2803eef617a1b92415ec5127fcd9c81322ad150
tangweichang <tangweichangjlu@gmail.com>
2015-09-22 02:07:04 -0400

    Add annotations with label and add them to my memorable places in the table view

--- Memorable Places/ViewController.swift
+++ Memorable Places/ViewController.swift
@@ -7,12 +7,89 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    var manager:CLLocationManager!
 
+    @IBOutlet var map: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        manager = CLLocationManager()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        if activePlace == -1 {
+            manager.requestWhenInUseAuthorization()
+            manager.startUpdatingLocation()
+        } else {
+            let latitude = NSString(string:places[activePlace]["lat"]!).doubleValue
+            let longitude = NSString(string:places[activePlace]["lon"]!).doubleValue
+            var latDelta:CLLocationDegrees = 0.01
+            var lonDelta:CLLocationDegrees = 0.01
+            var coordinate = CLLocationCoordinate2DMake(latitude, longitude)
+            var span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
+            var region:MKCoordinateRegion = MKCoordinateRegionMake(coordinate, span)
+            self.map.setRegion(region, animated: true)
+            var annotation = MKPointAnnotation()
+            annotation.coordinate = coordinate
+            annotation.title = places[activePlace]["name"]
+            self.map.addAnnotation(annotation)
+        }
+        
+        var uilpgr = UILongPressGestureRecognizer(target: self, action: "action:")
+        uilpgr.minimumPressDuration = 1.0
+        map.addGestureRecognizer(uilpgr)
+    }
+    
+    func action(gestureRecognizer:UIGestureRecognizer) {
+        if gestureRecognizer.state == UIGestureRecognizerState.Began {
+            var touchPoint = gestureRecognizer.locationInView(self.map)
+            var newCoordinate = map.convertPoint(touchPoint, toCoordinateFromView: self.map)
+            
+            var location = CLLocation(latitude: newCoordinate.latitude, longitude: newCoordinate.longitude)
+            CLGeocoder().reverseGeocodeLocation(location, completionHandler: { (placemarks, error) -> Void in
+                var title = ""
+                if error == nil {
+                    if let p:CLPlacemark = CLPlacemark(placemark: placemarks![0]) {
+                        var subThoroughfare:String = ""
+                        var thoroughfare:String = ""
+                        if p.subThoroughfare != nil {
+                            subThoroughfare = p.subThoroughfare!
+                        }
+                        if p.thoroughfare != nil {
+                            thoroughfare = p.thoroughfare!
+                        }
+                        title = "\(subThoroughfare) \(thoroughfare)"
+                    }
+                }
+                if title == "" {
+                    title = "Added \(NSDate())"
+                    
+                }
+            
+            
+            places.append(["name":title,"lat":"\(newCoordinate.latitude)","lon":"\(newCoordinate.longitude)"])
+            var annotation = MKPointAnnotation()
+            annotation.coordinate = newCoordinate
+            annotation.title = title
+            self.map.addAnnotation(annotation)
+        })
+    }
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        var userLocation:CLLocation = locations[0]
+        var latitude = userLocation.coordinate.latitude
+        var longitude = userLocation.coordinate.longitude
+        var latDelta:CLLocationDegrees = 0.01
+        var lonDelta:CLLocationDegrees = 0.01
+        var coordinate = CLLocationCoordinate2DMake(latitude, longitude)
+        var span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
+        var region:MKCoordinateRegion = MKCoordinateRegionMake(coordinate, span)
+        self.map.setRegion(region, animated: true)
+        
     }
 
     override func didReceiveMemoryWarning() {
